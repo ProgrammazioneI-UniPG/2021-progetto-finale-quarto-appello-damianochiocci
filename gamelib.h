37f583b6f033221aa27421a45b6ec85b0526d9f8
damianochiocci <damiano.chiocci@studenti.unipg.it>
2021-04-11 10:59:15 +0200

    Add files via upload

--- gamelib.h
+++ gamelib.h
@@ -1,5 +1,29 @@
-// Dichiarazione delle funzioni da chiamare in main.c
-// Definizione dei tipi necessari alla libreria
+#include <stdbool.h>
+#include <stdio.h>
+bool confirm_setting;
+int choice;
+bool end_of_game;
 
+enum Player_name {Red, Blue, Green, Cyan, Brown, Black, Dark_Green, White, Pink, Violet};
+enum Player_status {Astronaut, Impostor, Murdered, Ejected};
+enum Type_Room {Empty, Easy_quest, Hard_quest, Trapdoor};
 
+struct Player {
+  struct Room *Position;
+	enum Player_status Status;
+	enum Player_name Name;
+};
 
+struct Room {
+	struct Room *Forward;
+	struct Room *Right;
+	struct Room *Left;
+	struct Room *Previous_room;
+  struct Room *Next;
+  enum Type_Room type;
+  bool emergency_call;
+};
+
+void Set_Game();
+void Play();
+void End_Game();
--- main.c
+++ main.c
@@ -1,13 +1,54 @@
 #include "gamelib.h"
 
-// Questo file continene solo la definizione della funzione main
-// con il menu principale 1-2-3
-
-int main( ) {
-
-
-
+int main() {
+  printf("*-*-*-*-*-WELCOME TO...TRANNOI!-*-*-*-*-*\n");
+  do {
+      if (!confirm_setting) {
+        printf("\n1) Set Game\n2) Play\n3) End Game\n");
+        scanf("%d", &choice);
+        while (getchar()!='\n');
+        switch(choice) {
+          case 1:
+            Set_Game();
+            break;
+          case 2:
+            printf ("You haven't already set the game!\n");
+            break;
+          case 3:
+            End_Game();
+            break;
+          default:
+            printf("Choice not suitable!\n");
+            break;
+        }
+      } else if (confirm_setting && !end_of_game) {
+        printf("You set the game, now you can:\n2) Play\n3) End Game\n");
+        scanf("%d", &choice);
+        while (getchar()!='\n');
+        switch(choice) {
+          case 2:
+            Play();
+            break;
+          case 3:
+            End_Game();
+            break;
+          default:
+            printf("Choice not suitable!\n");
+            break;
+        }
+      } else if (end_of_game) {
+        printf ("The game is finished, press 3 to end game\n");
+        scanf("%d", &choice);
+        while (getchar()!='\n');
+        switch (choice) {
+          case 3:
+            End_Game();
+            break;
+          default:
+            printf ("Choice not suitable!\n");
+            break;
+        }
+      }
+  } while(choice !=3);
+  return 0;
 }
-
-
-
